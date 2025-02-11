bd02cd5ac0c518bd75f7825dc23d2af9c02fcb89
Gerald Soosairaj <asoosairaj@ucsd.edu>
2025-02-11 06:53:40 -0800

    Add header file.

---
+++ math_utils.h
@@ -0,0 +1,11 @@
+#ifndef MATH_UTILS_H
+#define MATH_UTILS_H
+
+double square(double x);
+double cube(double x);
+long power(int base, int exponent);
+long factorial(int n);
+
+extern int operation_count;
+
+#endif /* MATH_UTILS_H */
